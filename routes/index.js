7e19941b9e20b5f100874e8d7b7e0abe6a56b93e
Alex <alexbartley2@gmail.com>
2016-03-29 10:32:16 +1300

    Can now browse all xml files, and open xml in a new page. Can also search using plain text.

--- routes/index.js
+++ routes/index.js
@@ -21,21 +21,34 @@ router.get("/", function(req,res) {
 /* GET search page. */
 router.get("/Search", function(req,res) {
 	var queries = req.query;
-	client.execute(tei_path + 
-	"for $n in (" + queries.mysearch + ") return db:path($n)",
+	//client.execute(tei_path + "for $n in .//[. contains text " + queries.mysearch + "] return db:path($n)",
+	client.execute(tei_path + "for $n in //text where matches($n, '" + queries.mysearch + "', 'i') = true() return db:path($n)",
 	function (error, result) {
 		if(error){ console.error(error);}
 		else {
-			res.render('index', { title: 'Colenso search results', place: result.result.split('\n') });
+			res.render('search', { title: 'Colenso search results', query_results: result.result.split('\n'), query_term: queries.mysearch });
 		}
 	}
 	);
 });
 
-/* GET browse page. */
+/* GET advanced search page. */
+router.get("/XquerySearch", function(req,res) {
+	var queries = req.query;
+	client.execute(tei_path + "for $n in (" + queries.mysearch + ") return db:path($n)",
+	function (error, result) {
+		if(error){ console.error(error);}
+		else {
+			res.render('XquerySearch', { title: 'Colenso search results', place: result.result.split('\n'), query_term: queries.mysearch });
+		}
+	}
+	);
+});
+
+/* GET browse page. (Load all XML paths) */
 router.get("/Browse", function(req,res) {
 	var queries = req.query;
-	client.execute("LIST Colenso",
+	client.execute(tei_path + "for $n in //text where matches($n, '" + " " + "', 'i') = true() return db:path($n)",
 	function (error, result) {
 		if(error){ console.error(error);}
 		else {
@@ -45,16 +58,14 @@ router.get("/Browse", function(req,res) {
 	);
 });
 
-/* GET home page. */
+/* GET database XML files. */
 router.get("/Colenso/*", function(req,res) {
 	var url = req.params[0];
-	client.execute("XQUERY " + 
-	"doc('Colenso/" + url + "')",
+	client.execute("XQUERY " + "doc('Colenso/" + url + "')",
 	function (error, result) {
 		if(error){ console.error(error);}
 		else {
-			res.set('Content-Type', 'text/xml');
-			res.send(result.result);
+			res.render('viewXML', { title: url, xml: result.result });
 		}
 	}
 	);
