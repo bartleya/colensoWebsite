73058fcf0838937a17884b9cb72b291f612110c6
Alex <alexbartley2@gmail.com>
2016-03-30 15:31:32 +1300

    Can plain text search using logical operators. Placeholder XML download button added.

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get("/", function(req,res) {
 	function (error, result) {
 		if(error){ console.error(error);}
 		else {
-			res.render('index', { title: 'Colenso search results', place: result.result.split('\n') });
+			res.render('index', { title: 'The Colenso project' });
 		}
 	}
 	);
@@ -21,38 +21,50 @@ router.get("/", function(req,res) {
 /* GET search page. */
 router.get("/Search", function(req,res) {
 	var queries = req.query;
-	//client.execute(tei_path + "for $n in .//[. contains text " + queries.mysearch + "] return db:path($n)",
-	client.execute(tei_path + "for $n in //text where matches($n, '" + queries.mysearch + "', 'i') = true() return db:path($n)",
-	function (error, result) {
-		if(error){ console.error(error);}
-		else {
-			res.render('search', { title: 'Colenso search results', query_results: result.result.split('\n'), query_term: queries.mysearch });
+	if (queries.mysearch==null || queries.mysearch==false) {
+		//If this is the first time visiting the page, there won't have been a search yet.
+		console.log("Null or false");
+		res.render('search', { title: 'Colenso plain text search', query_results: false, query_term: "" });
+	} else {
+			client.execute(tei_path + "for $n in (/TEI[. contains text " + queries.mysearch + "]) return db:path($n)",
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				res.render('search', { title: 'Colenso search results', query_results: result.result.split('\n'), query_term: queries.mysearch });
+			}
 		}
+		);
 	}
-	);
+
 });
 
 /* GET advanced search page. */
 router.get("/XquerySearch", function(req,res) {
 	var queries = req.query;
-	client.execute(tei_path + "for $n in (" + queries.mysearch + ") return db:path($n)",
-	function (error, result) {
-		if(error){ console.error(error);}
-		else {
-			res.render('XquerySearch', { title: 'Colenso search results', place: result.result.split('\n'), query_term: queries.mysearch });
+	if (queries.mysearch==null || queries.mysearch==false) {
+		//If this is the first time visiting the page, there won't have been a search yet.
+		console.log("Null or false");
+		res.render('XquerySearch', { title: 'Colenso Xquery search', query_results: false, query_term: "" });
+	} else {
+		client.execute(tei_path + "for $n in (" + queries.mysearch + ") return db:path($n)",
+		function (error, result) {
+			if(error){ console.error(error);}
+			else {
+				res.render('XquerySearch', { title: 'Colenso search results', query_results: result.result.split('\n'), query_term: queries.mysearch });
+			}
 		}
+		);
 	}
-	);
 });
 
 /* GET browse page. (Load all XML paths) */
 router.get("/Browse", function(req,res) {
 	var queries = req.query;
-	client.execute(tei_path + "for $n in //text where matches($n, '" + " " + "', 'i') = true() return db:path($n)",
+	client.execute(tei_path + "for $n in //text where matches($n, ' ', 'i') = true() return db:path($n)",
 	function (error, result) {
 		if(error){ console.error(error);}
 		else {
-			res.render('index', { title: 'Colenso search results', place: result.result.split('\n') });
+			res.render('browse', { title: 'Browse the Colenso database', query_results: result.result.split('\n') });
 		}
 	}
 	);
@@ -60,12 +72,26 @@ router.get("/Browse", function(req,res) {
 
 /* GET database XML files. */
 router.get("/Colenso/*", function(req,res) {
-	var url = req.params[0];
-	client.execute("XQUERY " + "doc('Colenso/" + url + "')",
+	var url = "Colenso/" + req.params[0];
+	client.execute("XQUERY " + "doc('" + url + "')",
 	function (error, result) {
 		if(error){ console.error(error);}
 		else {
-			res.render('viewXML', { title: url, xml: result.result });
+			res.render('viewXML', { title: req.params[0], xml: result.result, url: url });
+		}
+	}
+	);
+});
+
+/* DOWNLOAD database XML files. */
+router.get("/Download/*", function(req,res) {
+	var url = req.params[0];
+	client.execute("XQUERY doc('" + url + "')",
+	function (error, result) {
+		if(error){ console.error(error);}
+		else {
+			res.set('Content-Type', 'text/xml');
+			res.send(result.result);
 		}
 	}
 	);
